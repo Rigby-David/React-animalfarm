936136f6757c774567503501d6391f1e22c9eb64
David <Rigby335@hotmail.com>
2022-06-07 14:18:01 -0700

    added Footer and Header, working on Main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Footer from './Footer/Footer.js';
+import Header from './Header/Header.js';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+      <Header greeting='farmer!' />
+      <Footer email='rigby335@hotmail.com'/>
     </div>
   );
 }
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import AnimalCard from '../Animal/Animal';
 import { animals } from '../data.js';
+import backgroundImage from '../background.png';
 
 export default function Main() {
   return (
-    <main>
+    <main style={{ backgroundImage: `url(${backgroundImage})` }}>
       {animals.map((zodiac, i) => (
         <AnimalCard {...animals} key={animals.name + i} />
       ))}
